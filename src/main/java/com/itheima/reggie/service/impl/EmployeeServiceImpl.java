365015a0889803b92fd93f58be8f160a92796a12
kingsley <2367348208@qq.com>
2023-12-27 21:50:44 +0800

    your commit message

--- src/main/java/com/itheima/reggie/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.mapper.EmployeeMapper;
 import com.itheima.reggie.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
 
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
 }
